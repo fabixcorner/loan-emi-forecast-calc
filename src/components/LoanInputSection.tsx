e87260ab9f84beea0d1a8008868f446d11dbc8a7
FabixCorner <fabixcorner@gmail.com>
2025-11-18 10:23:44 +0530

    Update year range in loan input section
    Update year range in loan input section

--- src/components/LoanInputSection.tsx
+++ src/components/LoanInputSection.tsx
@@ -52,7 +52,7 @@ export const LoanInputSection = ({
   const getYearOptions = () => {
     const currentYear = new Date().getFullYear();
     const years = [];
-    for (let year = 2001; year <= currentYear + 30; year++) {
+    for (let year = 2010; year <= currentYear + 40; year++) {
       years.push(year);
     }
     return years;
